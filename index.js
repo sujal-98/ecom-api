610648784b1423ee7c8f55e790c36f4314910d1e
sujal <sujalgupta1412@gmail.com>
2024-04-11 19:41:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express=require('express');
+const app=express();
+const dotenv=require('dotenv')
+dotenv.config();
+const mongoose=require('mongoose')
+const userRoute=require('./routes/user')
+const authRoute=require('./routes/auth')
+const prodRoute=require('./routes/prod')
+const carRoute=require('./routes/car')
+const orderRoute=require('./routes/ord')
+
+
+
+mongoose.connect(process.env.url
+).then(()=>{
+    console.log("Db connection successfull"
+    )
+}).catch((err)=>{
+    console.log(err);
+})
+
+
+app.get('/api',()=>{
+    console.log("yo baby")
+})
+
+//middlewares
+
+app.use(express.json())
+app.use("/api/auth",authRoute)
+app.use("/api/user",userRoute)
+app.use("/api/prod",prodRoute)
+app.use("/api/car",carRoute)
+app.use("/api/ord",orderRoute)
+
+
+app.listen(5000,()=>{
+    console.log("server running");
+})
